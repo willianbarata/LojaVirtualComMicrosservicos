5f57c5ac4151ac36db13c20fbca8911d9678da42
willianbarata <willianbarata@gmail.com>
2023-06-15 19:38:36 -0300

    Adicionar arquivos de projeto.

---
+++ Ternos.ProductApi/Repositories/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ternos.ProductApi.Models;
+
+namespace Ternos.ProductApi.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAll();
+        Task<IEnumerable<Category>> GetCategoriesProducts();
+        Task<Category> GetById(int id);
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Delete(int id);
+
+    }
+}
---
+++ Ternos.ProductApi/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ternos.ProductApi.Models;
+
+namespace Ternos.ProductApi.Repositories
+{
+    public interface IProductRepository 
+    {
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> GetById(int id);
+        Task<Product> Create(Product category);
+        Task<Product> Update(Product category);
+        Task<Product> Delete(int id);
+    }
+}
